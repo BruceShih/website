5872a3e5b789a82d368dcb668c3176ba922bdab3
Bruce Shih <sp1986@gmail.com>
2024-04-09 19:43:37 +0800

    fix: zero config to deploy is a lie

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,10 @@ import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
   devtools: { enabled: process.env.NODE_ENV !== 'production' },
+  routeRules: {
+    // prerender index route by default otherwise vercel will give you 500
+    '/': { prerender: true }
+  },
   modules: [
     '@nuxtjs/eslint-module',
     '@vueuse/nuxt',
