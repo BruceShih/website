ced8cf1c0dcb2668196339d54473cbb1d253b7f6
Bruce Shih <sp1986@gmail.com>
2024-05-16 18:21:27 +0800

    feat: redesign blog list page

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -14,6 +14,16 @@ const stories = useState<typeof response.data.stories>('blogs', () => response.d
 useHead({
   title: 'Blog'
 })
+
+function getFirstParagraph(story: typeof response.data.stories[0]) {
+  const firstParagraph = story.content.content.content?.find(block => block.type === 'paragraph')
+  if (firstParagraph) {
+    const firstText = firstParagraph.content?.find(e => e.type === 'text')
+    if (firstText)
+      return firstText.text
+  }
+  return ''
+}
 </script>
 
 <template>
@@ -23,32 +33,31 @@ useHead({
     </h1>
     <div class="py-6">
       <template v-for="(story, index) in stories" :key="story.id">
-        <UCard>
-          <template #header>
-            <h2 class="text-xl">
-              {{ story.content.title }}
-            </h2>
-          </template>
-
-          <p class="text-primary flex justify-end">
-            <NuxtLink v-if="story.full_slug" :to="story.full_slug">
-              ...Keep reading
-            </NuxtLink>
-          </p>
-
-          <template #footer>
-            <div class="flex justify-between">
-              <span class="mr-4 text-base-content text-sm">
-                <UseTimeAgo
-                  v-slot="{ timeAgo }"
-                  :time=" story.created_at"
-                >
-                  {{ timeAgo }}
-                </UseTimeAgo>
-              </span>
-            </div>
-          </template>
-        </UCard>
+        <h2 class="text-xl mb-2">
+          {{ story.content.title }}
+        </h2>
+        <p class="mb-2">
+          {{ getFirstParagraph(story) }}
+        </p>
+        <div class="flex justify-between">
+          <span class="mr-4 text-base-content text-sm">
+            <UseTimeAgo
+              v-slot="{ timeAgo }"
+              :time=" story.created_at"
+            >
+              {{ timeAgo }}
+            </UseTimeAgo>
+          </span>
+          <ULink
+            v-if="story.full_slug"
+            class="text-sm"
+            active-class="text-primary"
+            inactive-class="text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200"
+            :to="story.full_slug"
+          >
+            ...Keep reading
+          </ULink>
+        </div>
         <UDivider v-if="index !== stories.length - 1" type="solid" />
       </template>
     </div>
