71d25c8b5ea17024fb0885b9d91c0fc56ccd9831
Bruce Shih <sp1986@gmail.com>
2024-08-20 21:40:21 +0800

    chore: remove comments

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,5 +1,4 @@
 export default defineAppConfig({
-  // Configure Content Wind here
   socials: {
     twitter: 'bruceshihtw',
     github: 'BruceShih'
--- app/app.vue
+++ app/app.vue
@@ -1,8 +1,6 @@
 <template>
   <NuxtLoadingIndicator />
   <NuxtPage />
-  <!-- <NuxtLayout>
-  </NuxtLayout> -->
 </template>
 
 <style scoped>
