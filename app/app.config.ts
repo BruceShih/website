a698176303702525a692ffc716d7e0964c20f13d
Bruce Shih <sp1986@gmail.com>
2024-08-20 20:56:16 +0800

    feat: migrate to nuxt content

---
+++ app/app.config.ts
@@ -0,0 +1,7 @@
+export default defineAppConfig({
+  // Configure Content Wind here
+  socials: {
+    twitter: 'bruceshihtw',
+    github: 'BruceShih'
+  }
+})
---
+++ app/components/AppFooter.vue
@@ -0,0 +1,3 @@
+<template>
+  built with Nuxt Content
+</template>
