34c5fb392b7639078acfb8c867f33e8a482b3ad9
Bruce Shih <sp1986@gmail.com>
2025-09-10 12:46:54 +0800

    chore: update cf compatibility date

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -26,8 +26,7 @@ export default defineNuxtConfig({
     }
   },
   // https://nuxt.com/docs/getting-started/upgrade#testing-nuxt-4
-  future: { compatibilityVersion: 4 },
-  compatibilityDate: '2025-03-01',
+  compatibilityDate: '2025-04-25',
   // https://hub.nuxt.com/docs/getting-started/installation#options
   hub: {},
   vite: {
